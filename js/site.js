3df4644f11df3218d84ecf9f7e6a9f392bb7ffb1
Cédric Vigne <cedric.vigne11@gmail.com>
2016-03-15 09:10:12 +0100

    ajout event quand clic sur submit

--- js/site.js
+++ js/site.js
@@ -3,8 +3,8 @@
 $(document).ready(function(){
 	var resultSearch = $('.resultSearch'), logo = $('.brand-logo'), header = $('.page-header'), footer = $('.page-footer'), footerText = $('.page-footer .container'), map = $('#map'), displayHeight = $(window).height();
 	$(header).css('height', 0.1*displayHeight+'px');
-	$(footer).css('height', 0.1*displayHeight+'px');
-	$(map).css('height', 0.8*displayHeight+'px');
+	$(footer).css('height', 0.05*displayHeight+'px');
+	$(map).css('height', 0.85*displayHeight+'px');
 	$(resultSearch).css('height', 0.25*displayHeight+'px');
 	$(resultSearch).css('bottom', 0.115*displayHeight+'px');
 
@@ -14,12 +14,20 @@ $(document).ready(function(){
 	$(window).resize(function(){
 		var resultSearch = $('.resultSearch'), logo = $('.brand-logo'), header = $('.page-header'), footer = $('.page-footer'), footerText = $('.page-footer .container'), map = $('#map'), displayHeight = $(window).height();
 		$(header).css('height', 0.1*displayHeight+'px');
-		$(footer).css('height', 0.1*displayHeight+'px');
-		$(map).css('height', 0.8*displayHeight+'px');
+		$(footer).css('height', 0.05*displayHeight+'px');
+		$(map).css('height', 0.85*displayHeight+'px');
 		$(resultSearch).css('height', 0.25*displayHeight+'px');
 		$(resultSearch).css('bottom', 0.115*displayHeight+'px');
 
 		$(logo).css({height: $(header).height()-10+'px', marginTop: '5px', marginBottom: '5px'});
 		$(footerText).css('line-height', $(footer).height()+'px');
+	});
+
+
+	//Provisoire
+	var SearchSubmit = $('.inputsubmit');
+	SearchSubmit.click(function(){
+		event.preventDefault();
+		resultSearch.fadeIn(500);
 	});
 });
